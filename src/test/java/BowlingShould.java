a12787a56a4f50dd1269f1ee1f1287d9c1a68d10
Wolfero <ferorock44@gmail.com>
2022-05-16 13:49:16 +0100

    🔴 miss_all_pins_in_all_rolls

--- src/test/java/BowlingShould.java
+++ src/test/java/BowlingShould.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BowlingShould {
+    @Test
+    public void miss_all_pins_in_all_rolls() {
+        var game = new Bowling();
+
+        game.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        assertThat(game.finalScore()).isEqualTo(0);
+    }
 }
