0619f4a6661b016a2d91d27c0e4de47aa646e5d1
Wolfero <ferorock44@gmail.com>
2022-05-16 14:32:20 +0100

    ♻Refactor

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,13 +1,15 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class BowlingShould {
+public class BowlingGameShould {
     @Test
     public void miss_all_pins_in_all_rolls() {
-        var game = new Bowling();
+        var game = new Game();
 
-        game.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        game.roll(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
 
         assertThat(game.finalScore()).isEqualTo(0);
     }
