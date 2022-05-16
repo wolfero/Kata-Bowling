d54df2d25a512d312974ca32f73c46780e0093cc
Wolfero <ferorock44@gmail.com>
2022-05-16 14:30:59 +0100

    🟢 miss_all_pins_in_all_rolls

---
+++ src/main/java/Game.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    public void roll(List<Integer> rolls) {
+        for (int pinsDown : rolls) {
+            saveRoll(pinsDown);
+        }
+    }
+
+    private final List<Integer> rolls = new ArrayList<>();
+
+    private void saveRoll(int pinsDown) {
+        rolls.add(pinsDown);
+    }
+
+    public int finalScore() {
+        return 0;
+    }
+}
