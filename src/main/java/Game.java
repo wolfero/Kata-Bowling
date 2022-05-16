1b40f62db87b36a7b4d21135182403547626230b
Wolfero <ferorock44@gmail.com>
2022-05-16 16:49:19 +0100

    🟢 make_real_game_rolls_and_get_theyr_score

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,45 +2,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    public void roll(List<Integer> rolls) {
-        for (int pinsDown : rolls) {
-            saveRoll(pinsDown);
-        }
-    }
-
     private final List<Integer> rolls = new ArrayList<>();
 
-    private void saveRoll(int pinsDown) {
+
+    public void saveRoll(int pinsDown) {
         rolls.add(pinsDown);
     }
 
-    private final int maxFrames = 10;
-    private final int strike = 10;
-
     public int finalScore() {
         var score = 0;
-        var currentRoll = 0;
-        for (int frame = 0; frame < maxFrames; frame++) {
-            if (isStrike(currentRoll)) {
-                score += strike + rollScore(currentRoll + 1) + rollScore(currentRoll + 2);
-                currentRoll++;
-            } else if (isSpare(currentRoll)) {
-                score += strike + rollScore(currentRoll + 2);
-                currentRoll += 2;
+        int rollIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rollIndex)) {
+                score += strike(rollIndex);
+                rollIndex++;
+                continue;
             }
+
+            var frameScore = sumFrameScore(rollIndex);
+            if (isSpare(frameScore)) {
+                score += spare(rollIndex);
+            } else {
+                score += frameScore;
+            }
+
+            rollIndex += 2;
         }
         return score;
     }
 
-    private boolean isSpare(int currentRoll) {
-        return rollScore(currentRoll) + rollScore(currentRoll + 1) == strike;
-    }
-
     private boolean isStrike(int currentRoll) {
-        return rollScore(currentRoll) == strike;
+        return rolls.get(currentRoll) == 10;
     }
 
-    private Integer rollScore(int currentRoll) {
-        return rolls.get(currentRoll);
+    private int strike(int rollIndex) {
+        return 10 + rolls.get(rollIndex + 1) + rolls.get(rollIndex + 2);
+    }
+
+    private int sumFrameScore(int rollIndex) {
+        return rolls.get(rollIndex) + rolls.get(rollIndex + 1);
+    }
+
+    private boolean isSpare(int frameScore) {
+        return frameScore == 10;
+    }
+
+    private int spare(int rollIndex) {
+        return 10 + rolls.get(rollIndex + 2);
     }
 }
